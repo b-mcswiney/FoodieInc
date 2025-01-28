d2ddf8114b3063640f761ab3c20b27d23bc524f0
b-mcswiney <brooklynmcswiney@gmail.com>
2025-01-28 12:01:47 +0000

    made users table

---
+++ foodie/src/main/java/com/backend/foodie/entity/Users.java
@@ -0,0 +1,97 @@
+package com.backend.foodie.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	
+	@Column(length=150)
+	String username;
+	
+	@Column(length=250)
+	String password;
+	
+	@Column(length=150)
+	String email;
+	
+	@Column(length=150)
+	String address;
+
+	public Users() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Users(String username, String password, String email, String address) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.address = address;
+	}
+
+	public Users(int id, String username, String password, String email, String address) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.address = address;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", username=" + username + ", password=" + password + ", email=" + email
+				+ ", address=" + address + "]";
+	}
+}
