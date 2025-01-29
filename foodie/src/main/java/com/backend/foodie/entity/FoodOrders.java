2512d28661e5953aefe74594917b0422bbc4c492
b-mcswiney <brooklynmcswiney@gmail.com>
2025-01-29 10:27:34 +0000

    Added service for food orders

--- foodie/src/main/java/com/backend/foodie/entity/FoodOrders.java
+++ foodie/src/main/java/com/backend/foodie/entity/FoodOrders.java
@@ -11,6 +11,7 @@ import jakarta.persistence.ManyToOne;
 
 @Entity
 public class FoodOrders {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int id;
@@ -18,10 +19,70 @@ public class FoodOrders {
 	@ManyToOne
 	@JoinColumn(name="user_id")
 	Users user;
-
+	
 	@ManyToOne
 	@JoinColumn(name="restaurant_id")
 	Restaurants restaurant;
 	
 	LocalDateTime order_time;
+	
+	public FoodOrders() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FoodOrders(Users user, Restaurants restaurant, LocalDateTime order_time) {
+		super();
+		this.user = user;
+		this.restaurant = restaurant;
+		this.order_time = order_time;
+	}
+
+	public FoodOrders(int id, Users user, Restaurants restaurant, LocalDateTime order_time) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.restaurant = restaurant;
+		this.order_time = order_time;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Users getUser() {
+		return user;
+	}
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+
+	public Restaurants getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(Restaurants restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	public LocalDateTime getOrder_time() {
+		return order_time;
+	}
+
+	public void setOrder_time(LocalDateTime order_time) {
+		this.order_time = order_time;
+	}
+
+	@Override
+	public String toString() {
+		return "FoodOrders [id=" + id + ", user=" + user + ", restaurant=" + restaurant + ", order_time=" + order_time
+				+ "]";
+	}
 }
---
+++ foodie/src/main/java/com/backend/foodie/service/FoodOrderService.java
@@ -0,0 +1,42 @@
+package com.backend.foodie.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.backend.foodie.entity.FoodOrders;
+import com.backend.foodie.repo.FoodOrdersRepo;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class FoodOrderService {
+	@Autowired
+	private FoodOrdersRepo repo;
+	
+	public List<FoodOrders> getAllFoodOrders() {
+		return repo.findAll();
+	}
+	
+	public FoodOrders getById(int id) {
+		if(!repo.existsById(id))
+			throw new EntityNotFoundException("Cannot find "+id);
+		return repo.findById(id).get();
+	}
+	
+	public FoodOrders updateFoodOrder(FoodOrders toUpdate) {
+		if(!repo.existsById(toUpdate.getId()))
+			throw new EntityNotFoundException("Cannot find "+toUpdate.getId());
+		return repo.save(toUpdate);
+	}
+	
+	public boolean deleteFoodOrder(int id) {
+		if(!repo.existsById(id))
+			throw new EntityNotFoundException("Cannot find "+id);
+		
+		FoodOrders toDelete = repo.findById(id).get();
+		repo.delete(toDelete);
+		return true;
+	}
+}
